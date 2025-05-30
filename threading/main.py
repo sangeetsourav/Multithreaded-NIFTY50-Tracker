48a9e55fbfd75405798ad16a2ce1c8d6fe3867b9
Sunderroy, Sangeet <sangeet.sunderroy@ge.com>
2025-05-30 16:24:03 +0530

    Added a scheduler class and the threads now check a queue of symbols

--- threading/main.py
+++ threading/main.py
@@ -1,18 +1,36 @@
 from workers.WikiWorker import WikiWorker
-from workers.GoogleFinanceWorkers import GoogleFinancePriceWorker
+from workers.GoogleFinanceWorkers import GoogleFinancePriceScheduler
 import time
+from multiprocessing import Queue
+
+# Declare queue of symboles
+symbol_queue = Queue()
 
 scraper_start_time = time.time()
 
+# Wiki Worker instance
 wiki_worker = WikiWorker()
+
+# Get NIFTY 50 symbols
 wiki_worker.get_nifty_50()
 
-current_workers = []
-for symbol in wiki_worker.symbols:
-    google_finance_price_worker = GoogleFinancePriceWorker(symbol=symbol)
-    current_workers.append(google_finance_price_worker)
+# Google Scheduler instance
+num_google_finance_price_workers = 5
+google_finance_price_scheduler_threads = []
 
-for worker in current_workers:
+for i in range(num_google_finance_price_workers):
+    # Each instance starts running and waits for getting a value from the queue
+    google_finance_price_scheduler = GoogleFinancePriceScheduler(input_queue=symbol_queue)
+
+    google_finance_price_scheduler_threads.append(google_finance_price_scheduler)
+
+for symbol in wiki_worker.symbols:
+    symbol_queue.put(symbol)
+
+for worker in google_finance_price_scheduler_threads:
+    symbol_queue.put('DONE')
+
+for worker in google_finance_price_scheduler_threads:
     worker.join()
 
 print(f"Total extraction time: {round(time.time() - scraper_start_time,1)}")
--- threading/workers/GoogleFinanceWorkers.py
+++ threading/workers/GoogleFinanceWorkers.py
@@ -4,23 +4,37 @@ from lxml import html
 import time
 import random
 
-class GoogleFinancePriceWorker(threading.Thread):
-    def __init__(self, symbol, **kwargs):
+class GoogleFinancePriceScheduler(threading.Thread):
+    def __init__(self, input_queue, **kwargs):
         super().__init__(**kwargs)
+        self._input_queue = input_queue
+        self.start()
 
+    def run(self):
+        while True:
+            # get() keeps waiting until the queue returns value
+            # If the queue is initially empty then this method wil keep waiting
+            val = self._input_queue.get()
+            if val == 'DONE':
+                break
+
+            yahoo_finance_price_worker = GoogleFinancePriceWorker(symbol=val)
+            price = yahoo_finance_price_worker.get_price()
+            print(f"{val}: {price}")
+            time.sleep(random.random())
+
+class GoogleFinancePriceWorker:
+    def __init__(self, symbol):
         self._symbol = symbol
         self._base_url = 'https://www.google.com/finance/quote/'
         self._url = f"{self._base_url}{self._symbol}:NSE"
 
-        self.start()
-
-    def run(self):
-        # Start randomly after 0 to 30 s
-        time.sleep(30 * random.random())
+    def get_price(self):
         r = requests.get(self._url)
         if r.status_code != 200:
             return
         page_contents = html.fromstring(r.text)
         price_x_path = '//*[@id="yDmH0d"]/c-wiz[2]/div/div[4]/div/main/div[2]/div[1]/c-wiz/div/div[1]/div/div[1]/div/div[1]/div/span/div/div'
         price = float(page_contents.xpath(price_x_path)[0].text.replace('₹', '').replace(',',''))
-        print(price)
+
+        return price
